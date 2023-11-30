8ced1c917432f8685f2fbf05fdb9f95145e9ca06
emilyychau <emilychau.yvo@gmail.com>
2023-11-29 19:32:43 -0500

    rename custom element

--- custom.js
+++ custom.js
@@ -17,12 +17,14 @@ class CopyrightElement extends HTMLElement{
     }
 }
 
-customElements.define("x-footer", FooterElement);
+customElements.define("x-footer", CopyrightElement);
 
 // custom land acknowledgement message
 
-class FooterElement extends HTMLElement{
+class AcknowledgementElement extends HTMLElement{
     connectedCallback(){
         this.innerHTML = "I want to acknowledge the privilege I have to live and study on the territories traditionally occupied and cared for by Indigenous Peoples across Turtle Island. Laurier's campuses are located on the traditional territories of the Anishinabeg, Haudenosaunee and Attawandaron peoples. I am deeply grateful for the opportunity that I have to learn and grow on this land, which continues to be cared for by the First Peoples of Canada."
     }
 }
+
+customElements.define("x-footer", AcknowledgementElement);
