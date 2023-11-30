04a6a35da0c6a08a0a8e125f70f9aea68aad0fb3
emilyychau <emilychau.yvo@gmail.com>
2023-11-29 19:26:43 -0500

    update copyright custom element

--- custom.js
+++ custom.js
@@ -10,11 +10,19 @@ customElements.define("x-fullyear", YearElement);
 
 // custom footer content
 
-class FooterElement extends HTMLElement{
+class CopyrightElement extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = 
+        this.innerHTML = "designed with ♡ on treaty 4 & treaty 14 land"
         `<div style="color:DarkRed; text-align:center; font-family: Roboto, sans-serif;">${this.innerHTML}</div>`;
     }
 }
 
 customElements.define("x-footer", FooterElement);
+
+// custom land acknowledgement message
+
+class FooterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = "I want to acknowledge the privilege I have to live and study on the territories traditionally occupied and cared for by Indigenous Peoples across Turtle Island. Laurier's campuses are located on the traditional territories of the Anishinabeg, Haudenosaunee and Attawandaron peoples. I am deeply grateful for the opportunity that I have to learn and grow on this land, which continues to be cared for by the First Peoples of Canada."
+    }
+}
