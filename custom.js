ba94de75ed297cb5cc527e72c3a71ff3af7e9b35
emilyychau <emilychau.yvo@gmail.com>
2023-12-06 14:47:50 -0500

    update custom element

--- custom.js
+++ custom.js
@@ -12,8 +12,7 @@ customElements.define("x-fullyear", YearElement);
 
 class CopyrightElement extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = "designed with ♡ on treaty 4 & treaty 14 land"
-        `<div style="color:Black; text-align:center; font-family: Montserrat, sans-serif;">${this.innerHTML}</div>`;
+        this.innerHTML = "designed with ♡ on treaty 4 & treaty 14 land";
     }
 }
 
@@ -23,8 +22,7 @@ customElements.define("x-footer", CopyrightElement);
 
 class AcknowledgementElement extends HTMLElement{
     connectedCallback(){
-        this.innerHTML = "I want to acknowledge the privilege I have to live and study on the territories traditionally occupied and cared for by Indigenous Peoples across Turtle Island. Laurier's campuses are located on the traditional territories of the Anishinabeg, Haudenosaunee and Attawandaron peoples. I am deeply grateful for the opportunity that I have to learn and grow on this land, which continues to be cared for by the First Peoples of Canada."
-    `<div style=font-size: 0.8em; text-align: left;">${this.innerHTML}</div>`;
+        this.innerHTML = "I want to acknowledge the privilege I have to live and study on the territories traditionally occupied and cared for by Indigenous Peoples across Turtle Island. Laurier's campuses are located on the traditional territories of the Anishinabeg, Haudenosaunee and Attawandaron peoples. I am deeply grateful for the opportunity that I have to learn and grow on this land, which continues to be cared for by the First Peoples of Canada.";
     }
 }
 
